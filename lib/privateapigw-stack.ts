a2e1ede51ca4250dfbd7c522a91f0611b82c0041
Peter Chan <peterwt@amazon.com>
2021-07-28 15:27:13 +0800

    first commit

--- lib/privateapigw-stack.ts
+++ lib/privateapigw-stack.ts
@@ -2,17 +2,37 @@ import * as sns from '@aws-cdk/aws-sns';
 import * as subs from '@aws-cdk/aws-sns-subscriptions';
 import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as apigw from '@aws-cdk/aws-apigateway';
 
 export class PrivateapigwStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'PrivateapigwQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
+    const api = new apigw.RestApi(this, 'testPrivateApiAWS');
 
-    const topic = new sns.Topic(this, 'PrivateapigwTopic');
+    api.root.addMethod('ANY', new apigw.HttpIntegration('https://www.amazon.com/'));
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    api.root.addProxy({
+      anyMethod: true,
+      defaultIntegration: new apigw.HttpIntegration(
+        'https://www.amazon.com/{proxy}',
+        {
+          httpMethod: "GET",
+          options: {
+            requestParameters: {
+              "integration.request.path.proxy": "method.request.path.proxy",
+            },
+          },
+          proxy: true,
+        },
+      ),
+      defaultMethodOptions: {
+        methodResponses: [{ statusCode: "200" }],
+        requestParameters: {
+          "method.request.path.proxy": true,
+        },
+      },
+    })
+
   }
 }
